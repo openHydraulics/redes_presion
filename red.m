95235d184b3afcd334fcd00b8fc834f9f944eb07
Raúl Sánchez <raul.sanchez@upm.es>
2020-03-02 10:11:27 +0100

    Pérdidas de carga con caudal bidireccional y análisis energía a aportar en cabecera

--- red.m
+++ red.m
@@ -3,44 +3,90 @@
 %Enlace a funciones en carperta ./src/
 addpath('./src/');
 
-## H0, altura en cabeza de la red
+resultado=[];
+distBocas=[];
+
+## z0, cota del origen del agua
 ## D, diámetros de los tramos
 ## L, longitudes de los tramos
 ## k, aspereza de arena equivalente del material de los tubos de los tramos
 ## nu, viscosidad cinemática
 
-H0=50;D=transpose([0.29135 0.50463 0.65147 0.29135 0.29135]);L=transpose([1000 1000 1000 1000 1000]);k=transpose([4e-5 4e-5 4e-5 4e-5 4e-5]);nu=1.3e-6;
+z0=125;
+D=transpose([1.27662 0.78176 0.63831 0.45135 0.45135 0.78176 0.45135 0.45135]);
+L=transpose([1000 1000 1000 1000 1000 1000 1000 1000]);
+k=transpose([4e-5 4e-5 4e-5 4e-5 4e-5 4e-5 4e-5 4e-5]);
+nu=1.3e-6;
+
+## Caudal q y altura de presión h demandados en cada nodo (boca)
+q=transpose([0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1]).*(24/16*16/8);
+hreq=transpose([35 35 35 35 35 35 35 35]);
+
 
 ## x, abcisas de los nudos
 ## y, ordenadas de los nudos
 ## z, cotas de de los nudos
 
-x=transpose([3000 2000 1000 2000 1000]);
-y=transpose([0 0 0 -1000 -1000]);
-z=transpose([10 10 10 0 0 ]);
+x=transpose([1000 1000 2000 3000 1000 2000 3000 2000]);
+y=transpose([0 -1000 -1000 -1000 -2000 0 0 1000]);
+z=transpose([125 125 125 125 125 125 125 125]);
 
 ## Matriz de conexiones
 ## Tramos a recorrer hasta la boca i
-Mconex_hf(1,:)=[1 1 1 0 0];
-Mconex_hf(2,:)=[0 1 1 0 0];
-Mconex_hf(3,:)=[0 0 1 0 0];
-Mconex_hf(4,:)=[0 1 1 1 0];
-Mconex_hf(5,:)=[0 0 1 0 1];
+Mconex_hf(1,:)=[1 0 0 0 0 0 0 0];
+Mconex_hf(2,:)=[1 1 0 0 0 0 0 0];
+Mconex_hf(3,:)=[1 1 1 0 0 0 0 0];
+Mconex_hf(4,:)=[1 1 1 1 0 0 0 0];
+Mconex_hf(5,:)=[1 1 0 0 1 0 0 0];
+Mconex_hf(6,:)=[1 0 0 0 0 1 0 0];
+Mconex_hf(7,:)=[1 0 0 0 0 1 1 0];
+Mconex_hf(8,:)=[1 0 0 0 0 1 0 1];
 
 Mconex_Q=transpose(Mconex_hf);
 
-## Caudal q demandado en cada nodo (boca)
-q=transpose([0.1 0.1 0.1 0.1 0.1]);
+%Vector 1 al número de bocas
+vectorBocas=(1:1:8);
 
+for p=0.05:0.05:1
+  for i=1:100
+    %Caudal de cada tramo (tubería)
+    
+    ## Demanda cuando una boca abastece a uno sólo usuario
+    #qdemand=q.*(rand(8,1)>p);
+    
+    ## Demanda cuando una boca abastece a múltiples usuarios
+    qdemand=q.*p.*rand(8,1);
 
-%Caudal de cada tramo (tubería)
-Q=Mconex_Q*q;
+    H_nodo=hreq+z;
+     
+    Q=Mconex_Q*qdemand;
 
-%Pérdida de carga en cada tramo
-hf=IWC(Q,D,k,nu).*L; 
+    %Pérdida de carga en cada tramo
+     hf=IWC(Q,D,k,nu).*L;
 
-%Carga en cada nodo
-H=H0-Mconex_hf*hf;
+    %Carga en cada nodo
+    Hbombeo=H_nodo+Mconex_hf*hf-z0;
+      
+    if max(Q)>0
+      resultado=[resultado;max(Q) max(Hbombeo) vectorBocas*(Hbombeo==max(Hbombeo))];
+    endif
+    [p i]
+  endfor
+endfor
 
-%Altura de presión
-h=H-z;
+subplot(1,2,1)
+plot(resultado(:,1),resultado(:,2),'+')
+axis([0 max(resultado(:,1)) 0 max(resultado(:,2))])
+hold on
+plot([0 sum(q)],[min(resultado(:,2)) min(resultado(:,2))])
+hold off
+
+%resultado(lookup(resultado(:,2),max(resultado(:,2))),:)
+
+% Distribución de bocas que determinan la altura en cabeza
+for i=1:8
+  distBocas=[distBocas sum(resultado(:,3)==i)];
+endfor
+distBocas=distBocas./sum(distBocas);
+subplot(1,2,2)
+bar(distBocas)
--- src/IWC.m
+++ src/IWC.m
@@ -1,10 +1,13 @@
 %Cálculo de la pendiente motriz en tubos segun White-Colebrook
 %Q, D y k pueden ser vectores con el mismo numero de elementos
+% La función admite valores de Q negativos, cuyo significado es que el flujo se produciría en sentido contrario. Las pérdidas de carga tendrían también sentido contrario y serían por tanto negativas.
 
 function I=IWC(Q,D,k,nu)
-    I=1e-4*Q./Q ;Ia=2*I;
+    %Si Q es nulo, I también lo es
+    I=1e-4*abs(Q);
+    Ia=2*I;
   while abs(I-Ia)>1e-6
     Ia=I;
-    I=(4*Q/pi./D.^2./(-2*log10(k/3.7./D+2.51*nu./D./sqrt(2*9.80665*Ia.*D)))).^2/2/9.80665./D;
+    I=(4*abs(Q)/pi./D.^2./(-2*log10(k/3.7./D+2.51*nu./D./sqrt(2*9.80665*abs(Ia).*D)))).^2/2/9.80665./D.*(abs(Q)./Q);
   endwhile
 endfunction
